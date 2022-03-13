0988c38b0f64788c3ba88eeb9060948e0fba985f
Quilist <93136218+Quilist@users.noreply.github.com>
2022-03-13 15:11:57 +0200

    Add files via upload

--- src/pages/directory/PageСashAndAccounts.jsx
+++ src/pages/directory/PageСashAndAccounts.jsx
@@ -1,11 +1,11 @@
 import DocumentTitle from 'react-document-title'
 import Sidebar from "../../sidebar/Sidebar";
-import СashAndAccounts from '../../directory-components/directory/СashAndAccounts';
+import СashAndAccounts from '../../directory-components/directory/CashAndAccounts/СashAndAccounts';
 
 
 const PageСashAndAccounts = () => {
    return (
-      <DocumentTitle title='B-Fin: Справочник'>
+      <DocumentTitle title='B-Fin: Кассы и счета'>
          <div >
             <Sidebar />
             <СashAndAccounts />
