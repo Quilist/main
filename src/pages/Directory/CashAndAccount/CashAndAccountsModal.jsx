cbd87cb32faad48df8bea8b5f7747d74b7cdffd6
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-04 21:43:29 +0300

    Update CashAndAccountsModal.jsx

--- src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
+++ src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
@@ -130,6 +130,7 @@ export default function CashAndAccountsModal({ open, setOpen, auxiliaryList }) {
 
   const handleAdd = async () => {
     if (elem) {
+      console.log("OK")
       item.stream = {
         acc: elem.acc,
         balance: elem.balanceIn,
