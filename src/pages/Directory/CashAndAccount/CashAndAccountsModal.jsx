80bd1229b30ab32d42b1e14c226f5a09e52df8af
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-06 14:45:30 +0300

    Update CashAndAccountsModal.jsx

--- src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
+++ src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
@@ -296,7 +296,7 @@ export default function CashAndAccountsModal({ open, setOpen, auxiliaryList }) {
                           <Select
                             labelId="demo-simple-select-standard-label"
                             id="demo-simple-select-standard"
-                            value={item.cash_accounts_balance ? item.cash_accounts_balance[0].currency_id : ''}
+                            value={item.balance ? item.balance[0].currency_id : ''}
                             name="currency_id"
                             onChange={(e) => updateBalance(e)}
                           >
@@ -310,7 +310,7 @@ export default function CashAndAccountsModal({ open, setOpen, auxiliaryList }) {
                           label="Стартовый баланс:"
                           type="number"
                           variant="standard"
-                          value={item.cash_accounts_balance ? item.cash_accounts_balance[0].balance : ''}
+                          value={item.balance ? item.balance[0].balance : ''}
                           name="balance"
                           onChange={(e) => updateBalance(e)}
                         />
