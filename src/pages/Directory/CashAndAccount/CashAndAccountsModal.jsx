299ec23de0c5a3c7dfa0d988fc0a0a95753f73cd
Quilist <93136218+Quilist@users.noreply.github.com>
2022-05-19 18:02:28 +0300

    Update CashAndAccountsModal.jsx

--- src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
+++ src/pages/Directory/CashAndAccount/CashAndAccountsModal.jsx
@@ -233,9 +233,9 @@ export default function CashAndAccountsModal({ open, setOpen, auxiliaryList }) {
 
   const handleChangeDate = e => {
     const { name, value } = e.target;
-    const data = Number(e.nativeEvent.data) ? e.nativeEvent.data : undefined;
+    const data = Number.isFinite(Number(e.nativeEvent.data));
 
-    if (value.length > 10 || isNaN(data)) return;
+    if (value.length > 10 || !data) return;
 
     date[name] = value;
 
